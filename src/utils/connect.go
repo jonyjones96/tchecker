196b02e0beafc7c2235398c54443d68239a54a86
Jonathan Jones <jonyjones96@gmail.com>
2020-07-15 12:21:19 +0100

    added environment variable to dockerfile and updated subsequent db connection file

--- src/utils/connect.go
+++ src/utils/connect.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"context"
 	"fmt"
+	"os"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -17,16 +18,21 @@ var Client *mongo.Client
 
 func ConnectDB() error {
 
+	// Get the environment variable
+	host := os.Getenv("HOST_ENV")
+	username := os.Getenv("USER_ENV")
+	password := os.Getenv("PASS_ENV")
+
 	// May be of use: https://stackoverflow.com/questions/17588876/mongodb-conf-bind-ip-127-0-0-1-does-not-work-but-0-0-0-0-works
 	var (
 		client *mongo.Client
 		//mongoURL = "mongodb://mongo:27017"		// Needs to be used in docker container
-		mongoURL = "mongodb://127.0.0.1:27017" // Needs to be used for local dev
+		mongoURL = fmt.Sprintf("mongodb://%s:27017", host) // Needs to be used for local dev
 	)
 
 	fmt.Println("Initalising connection...")
 	// Initialize a new mongo client with options
-	client, err := mongo.NewClient(options.Client().ApplyURI(mongoURL).SetAuth(options.Credential{Username: "user", Password: "example"}))
+	client, err := mongo.NewClient(options.Client().ApplyURI(mongoURL).SetAuth(options.Credential{Username: username, Password: password}))
 
 	fmt.Println("Connecting to MongoDB Server...")
 	// Connect the mongo client to the MongoDB server
