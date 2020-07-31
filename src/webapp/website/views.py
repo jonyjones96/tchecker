a7aa9def62c59e83040d6f41decfa8086793ea92
Jonathan Jones <jonyjones96@gmail.com>
2020-07-31 23:05:04 +0100

    Dockerised app

--- src/webapp/website/views.py
+++ src/webapp/website/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
+import os
 
 def home(request):
-    response = requests.get('http://localhost:8080/tools')
+    print("Getting host")
+    host = os.environ['HOST_ENV']
+    print("Host: {host}")
+    response = requests.get('http://{}:8080/tools'.format(host))
     toolsJson = response.json() 
     return render(request, 'website/home.html', {'tools':toolsJson})
 
